40f4ee11ac291f4708ca4c48e87e98fe3706a8f6
Filip Rehorik <FilipRemoteIT@outlook.cz>
2020-10-20 11:54:03 +0200

    init

---
+++ AzureSearchAPI/AzureSearchAPI/Function1.cs
@@ -0,0 +1,60 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Azure;
+using Azure.Search.Documents;
+using Azure.Search.Documents.Models;
+
+namespace AzureSearchAPI
+{
+    public static class Function1
+    {
+        [FunctionName("Function1")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string name = req.Query["name"];
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            name = name ?? data?.name;
+
+
+            // Get the service endpoint and API key from the environment
+            Uri endpoint = new Uri(Environment.GetEnvironmentVariable("SEARCH_ENDPOINT"));
+            string key = Environment.GetEnvironmentVariable("SEARCH_API_KEY");
+            string indexName = "azureblob-index";
+
+            // Create a client
+            AzureKeyCredential credential = new AzureKeyCredential(key);
+            SearchClient client = new SearchClient(endpoint, indexName, credential);
+
+            // Let's get the top 5 related to Microsoft
+            SearchResults<SearchDocument> response = client.Search<SearchDocument>("Microsoft", new SearchOptions { Size = 5 });
+            foreach (SearchResult<SearchDocument> result in response.GetResults())
+            {
+                // Print out the title and job description (we'll see below how to
+                // use C# objects to make accessing these fields much easier)
+                string title = (string)result.Document["business_title"];
+                string description = (string)result.Document["job_description"];
+                Console.WriteLine($"{title}\n{description}\n");
+            }
+
+
+            string responseMessage = string.IsNullOrEmpty(name)
+                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+
+            return new OkObjectResult(responseMessage);
+        }
+    }
+}
